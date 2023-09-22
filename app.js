b1a3fbf857e949ce21a515da13bbf8c1743bda7d
francoromeo <francoromeo5012@gmail.com>
2023-09-22 11:35:19 -0300

    js

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log("welcome to Spotify");
+
+let songs = [
+    {
+        songName: "Salam-e-Ishq", filePath: "music/1.mp3", coverPath: "images/1.jpg"
+    }
+]
+
+let audioElement = new Audio('1.mp3');
